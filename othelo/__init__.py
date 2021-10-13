a6f4524be061108bdf2197f5b1d1f8b6dd3dab37
ssw**** <ssw3095@naver.com>
2021-10-13 14:55:00 +0000

    add readme.md

---
+++ othelo/__init__.py
@@ -0,0 +1,13 @@
+from othelo.board import Board
+from othelo.game import Game
+from othelo.piece import Piece
+from othelo.rule import Rule
+from othelo.user import User
+
+print(__name__)
+if __name__ == '__main__':
+    Board()
+    Game()
+    Piece()
+    Rule()
+    User()
---
+++ othelo/__main__.py
@@ -0,0 +1,12 @@
+from othelo.board import Board
+from othelo.game import Game
+from othelo.piece import Piece
+from othelo.rule import Rule
+from othelo.user import User
+
+if __name__ == '__main__':
+    print(Board)
+    print(Game)
+    print(Piece)
+    print(Rule)
+    print(User)
